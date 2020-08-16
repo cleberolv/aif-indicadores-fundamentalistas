408c46e4aa8d9a99c93d588eec1438c543516886
inmetrics <oliveira.bnhc@gmail.com>
2020-08-16 20:59:44 -0300

    Adding Valuation Runner

---
+++ src/test/java/application/aif/tests/ValuationTestRunner.java
@@ -0,0 +1,22 @@
+package application.aif.tests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		dryRun = false,
+		features = "src/test/resources/features/valuation.feature",
+		glue = "application.aif.steps",
+		monochrome = true,
+		plugin =  {"pretty", "html:target/reports/report-html", "json:target/reports/report-json/report.json"},
+		snippets = SnippetType.CAMELCASE,
+		strict = false,
+		tags = {"@Indicators"}
+		)
+public class ValuationTestRunner {
+
+}
